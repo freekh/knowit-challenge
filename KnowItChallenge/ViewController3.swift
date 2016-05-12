4354f5eaa78d10e45240e06b938f96528f1d26b0
Fredrik Ekholdt <freekh@gmail.com>
2016-05-13 01:02:10 +0200

    Forgotten cancel

--- KnowItChallenge/ViewController3.swift
+++ KnowItChallenge/ViewController3.swift
@@ -28,23 +28,26 @@ class ViewController3: UIViewController {
   @IBAction func executeCall(sender: AnyObject) {
     let phoneNumber = phoneNumberTextField.text! //closing over this later
     let alert = UIAlertController(title: "About to call", message: "You're about to call: \(phoneNumber)", preferredStyle: UIAlertControllerStyle.Alert)
-    alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: { action in
+    let alertHandler: UIAlertAction! -> Void = { action in
       switch action.style {
-        case .Default:
-          let urlEncoded = phoneNumber.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLPathAllowedCharacterSet())! //Not entirely sure this is the correct encoding
-          if let telUrl = NSURL(string: "tel://\(urlEncoded)") {
-            print("Opening url", telUrl)
-            UIApplication.sharedApplication().openURL(telUrl  )
-          } else {
-            //Could be handled by emitting a new alert
-            print("Failed", phoneNumber)
-          }
-        case .Cancel:
-          print("Cancel")
-        case .Destructive:
-          print("Destroy")
+      case .Default:
+        let urlEncoded = phoneNumber.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLPathAllowedCharacterSet())! //Not entirely sure this is the correct encoding
+        if let telUrl = NSURL(string: "tel://\(urlEncoded)") {
+          print("Opening url", telUrl)
+          UIApplication.sharedApplication().openURL(telUrl  )
+        } else {
+          //Could be handled by emitting a new alert
+          print("Failed", phoneNumber)
+        }
+      case .Cancel:
+        print("Cancel")
+      case .Destructive:
+        print("Destroy")
       }
-    }))
+    }
+    alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: alertHandler))
+    alert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: alertHandler))
+
     self.presentViewController(alert, animated: true, completion: nil)
   }
 }

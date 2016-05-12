84dcb66f9bcdd6e395ebdeb44dae7bad053f4475
Fredrik Ekholdt <freekh@gmail.com>
2016-05-13 01:13:16 +0200

    Indent and comments

--- KnowItChallenge/ViewController3.swift
+++ KnowItChallenge/ViewController3.swift
@@ -28,21 +28,22 @@ class ViewController3: UIViewController {
   @IBAction func executeCall(sender: AnyObject) {
     let phoneNumber = phoneNumberTextField.text! //closing over this later
     let alert = UIAlertController(title: "About to call", message: "You're about to call: \(phoneNumber)", preferredStyle: UIAlertControllerStyle.Alert)
+    //Thought it was neater to handle all actions in one place, but not sure it is necessarily better
     let alertHandler = { (action: UIAlertAction!) -> Void in
       switch action.style {
-      case .Default:
-        let urlEncoded = phoneNumber.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLPathAllowedCharacterSet())! //Not entirely sure this is the correct encoding
-        if let telUrl = NSURL(string: "tel://\(urlEncoded)") {
-          print("Opening url", telUrl)
-          UIApplication.sharedApplication().openURL(telUrl  )
-        } else {
-          //Could be handled by emitting a new alert
-          print("Failed", phoneNumber)
-        }
-      case .Cancel:
-        print("Cancel")
-      case .Destructive:
-        print("Destroy")
+        case .Default:
+          let urlEncoded = phoneNumber.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLPathAllowedCharacterSet())! //Not entirely sure this is the correct encoding
+          if let telUrl = NSURL(string: "tel://\(urlEncoded)") {
+            print("Opening url", telUrl)
+            UIApplication.sharedApplication().openURL(telUrl  )
+          } else {
+            //Could be handled by emitting a new alert
+            print("Failed", phoneNumber)
+          }
+        case .Cancel:
+          print("Cancel")
+        case .Destructive:
+          print("Destroy")
       }
     }
     alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: alertHandler))
